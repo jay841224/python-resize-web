e9d3c86858d611d92e135d7047e608133d717c11
jay841224 <j1360fy6416nb@gmail.com>
2020-05-03 12:06:38 +0800

    test

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from werkzeug.utils import secure_filename
 from google.cloud import storage
 import cv2
 import os, time
+import numpy as np
 
 app = Flask(__name__)
 CLOUD_STORAGE_BUCKET = os.environ['CLOUD_STORAGE_BUCKET']
@@ -30,15 +31,19 @@ def main():
 
         # Get the bucket that the file will be uploaded to.
         bucket = gcs.get_bucket(CLOUD_STORAGE_BUCKET)
-        bucket2 = gcs.get_bucket(CLOUD_STORAGE_BUCKET)
+        #bucket = gcs.get_bucket('my-resize-project')
 
         # Create a new blob and upload the file's content.
-        blob = bucket.blob(rootimg.filename)
+        blob = bucket.blob('root.jpg')
         blob.upload_from_string(rootimg.read(),content_type=rootimg.content_type)
-        read = bucket2.get_blob('mico.jpg')
-        '''
-        img = cv2.imread(uploadpath)
-        cv2.imwrite(os.path.join(basepath, 'static/images', 'test.jpg'), img) #save image
+        print(type(rootimg.read()))
+        
+        #read rootimg and get height and width
+        bucket = gcs.get_bucket('my-resize-project')
+        read = bucket.get_blob('root.jpg')
+        rootimg = read.download_as_string()
+        nparr = np.fromstring(rootimg, np.uint8)
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
         h, w, s = img.shape
         session['size'] = [w, h]
@@ -46,9 +51,7 @@ def main():
 
         #from work_img import resizeImg
         #img = resizeImg()
-        return render_template('main.html', val1=time.time(), maxwidth=rootSize[0], maxheight=rootSize[1])
-        '''
-        return render_template('main.html', root_img=read.public_url)
+        return render_template('main.html',root_img=blob.public_url, val1=time.time(), maxwidth=rootSize[0], maxheight=rootSize[1])
     return render_template('home.html')
 
 
@@ -57,8 +60,6 @@ def work():
     if request.method == 'POST':
         width = int(request.form['width'])
         height = int(request.form['height'])
-        basepath = os.path.dirname(__file__)
-        uploadpath = os.path.join(basepath, 'static/images', 'test.jpg')
 
         #決定html中 img的width height
         rootSize = session['size']
@@ -67,10 +68,26 @@ def work():
 
 
         from work_img import resizeImg
-        rootimg = cv2.imread(uploadpath)
-        img = resizeImg(rootimg, width, height)
-        cv2.imwrite(os.path.join(basepath, 'static/images', 'output.jpg'), img)
-        return render_template('main.html', val1=time.time(), val2=time.time(), outputWidthpersent=outputWidth, maxwidth=rootSize[0], maxheight=rootSize[1], outputsize=[width, height]) #max 限制resize的最大值 不可超出原圖
+        gcs = storage.Client()
+        bucket = gcs.get_bucket(CLOUD_STORAGE_BUCKET)
+        #bucket = gcs.get_bucket('my-resize-project')
+        read = bucket.get_blob('root.jpg')
+        rootimg = read.download_as_string()
+        nparr = np.fromstring(rootimg, np.uint8)
+        rootimg = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+        rimg = resizeImg(rootimg, width, height)
+        print(type(rimg))
+        s, rimg = cv2.imencode('.jpg', rimg)
+        rimg = rimg.tobytes()
+        bucket = gcs.get_bucket('my-resize-project')
+        blob = bucket.blob('rimg.jpg')
+        blob.upload_from_string(rimg)
+
+        return render_template('main.html', val1=time.time(), val2=time.time(), outputWidthpersent=outputWidth,
+        maxwidth=rootSize[0], maxheight=rootSize[1], outputsize=[width, height],
+        root_img=read.public_url, rimg=blob.public_url,
+        download_link=blob.media_link) #max 限制resize的最大值 不可超出原圖
     return jsonify('false')
 
 
